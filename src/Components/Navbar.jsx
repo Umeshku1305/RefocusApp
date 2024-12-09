052eb09ca0db9ae26edbcdd090a55b72ff8c7d7e
Umesh kumar mahto <umeshkumarmahto1305@gmail.com>
2024-12-09 12:04:08 +0530

    basic setup boilerplate

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="max-w-screen-xl p-3 mt-5 mx-auto bg-zinc-800 ">
+      <div className="">
+        <img className="" src="https://www.refokus.com/" alt="" />
+        <a href="#">Work</a>
+        <a href="#">News</a>
+        <a href="#">Home</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
